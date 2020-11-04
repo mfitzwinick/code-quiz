50d2f2cd547a8bc532734fc9b0501c95d9e9b7b5
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-04 11:36:12 -0800

    trying to add a function loop pushing text content into html elements (this is not working)

--- script.js
+++ script.js
@@ -9,15 +9,11 @@
 //HOW DO I ASSIGN SCORES?
 //IS INPUT FROM A PROMPT _ WHAT IS THE BEST WAY TO PULL IN SELECTION
 //3. After input, next question appears
-// 4. Wrong answers deduct 10 seconds from the clock
 // 5. Quiz ends when all questions are answered OR when time runs out.
-// 6. Quiz over when last question finished- 
+
 
 //CREATE A "REGISTER YOUR SCORE" BUTTON w/ move to local storage
 // High Scores into local storage
-//WINDOWS TO SHOW HIGH SCORES?
-//
-
 
 //set variables//
 var start = document.querySelector(".start")
@@ -45,7 +41,7 @@ start.addEventListener("click", function () {
     countDownText();
     start.style.display = 'none'
     platter.style.display = 'block'
-    populateQuestion();
+    next();
 });
 //Have timer start and end at 0//
 function countDownText() {
@@ -61,90 +57,99 @@ function countDownText() {
 }
 //create Q & A in objects//
 var questions = [
-    {q: "What is a dog?",
-    pickOne: [
-         "A piece of wood",
-         "your BFF",
-        "a chemical",
-        "a vacation"],
+    {
+        q: "What is a dog?",
+        pickOne: [
+            "A piece of wood",
+            "your BFF",
+            "a chemical",
+            "a vacation"],
         answers: "your BFF"
-}, 
-{q: "where is the giraffe",
-    pickOne: [
-        " a piece of wood",
-       "your BFF",
-        "a chemical",
-        "on vacation"]
+    },
+    {
+        q: "where is the giraffe",
+        pickOne: [
+            " a piece of wood",
+            "your BFF",
+            "a chemical",
+            "on vacation"],
         answers: "on vacation"
 
-}, 
-{q: "What is a hotdog?"
-pickOne: [
-     "gasfdsagsad",
-     "asdsaFF",
-    "aagsdical",
-    "a asdadaation"],
-    answers: "asdsaFF"
-}]
-//create for loop for questions//
+    },
+    {
+        q: "What is a hotdog?",
+        pickOne: [
+            "gasfdsagsad",
+            "asdsaFF",
+            "aagsdical",
+            "a asdadaation"],
+        answers: "asdsaFF"
+    }]
+//create for loop for questions//_--------------------------------------------
 
-function next (q, pickOne, answers) {
-    for (var i = 0; i<3; i++) {
-        questionBoxH1.textContent = q[i]
+function next(q, pickOne, answers) {
+    for (var i = 0; i < 3; i++) {
+        // var qBox =document.createElement("div")
+        qBox.textContent = q[i]
+        // questionBoxH1.appendChild (qBox)
     }
         
-        )
-    for(var i = 0; i < pickOne.length; i++) {
-        var liEl = document.createElement("button");
-        liEl.textContent = pickOne[i]
-        platter.appendChild(liEl)
-        if(pickOne[0] === answers) {
-            alert(`You chose correctly`)
-        } 
+    for (var i = 0; i < pickOne.length; i++) {
+        // var liEl = document.createElement("button");
+        choiceA.textContent = pickOne[i]
+        choiceB.textContent = pickOne[i]
+        choiceC.textContent = pickOne[i]
+        choiceD.textContent = pickOne[i]
+
+        // platter.appendChild(liEl)
+        if (pickOne[i] === answers) {
+            myScore.textContent = startingScore;
+            startingScore + 10;
+            alert("CORRECT!")
+        }
+    else {  timeRemaining - 10;
+        alert("INCORRECT ANSWER")
+
     }
-//     questions[0].pickOne
-// }
-
-//test(questions[0].pickOne, questions[0].answers)
-next (q[2].questions[2].pickOne, questions[2].answers)
-//create "click" function for Answer Choice//
-clickAnswer.addEventListener("click", function () {
-    next();
-    scoreTally();
-});
-
-// function populateQuestion(){
-//     questionBoxH1.textContent=questions[0].q;
-//     choiceA.textContent=questions[0].pickOne[0].select;
-//     choiceB.textContent=questions[0].pickOne[1].select;
-//     choiceC.textContent=questions[0].pickOne[2].select;
-//     choiceD.textContent=questions[0].pickOne[3].select;
-    
-// };
-
-function scoreTally () {
-    for (var i=0; i <questions.length; i++) {
     }
-    if (ans=== true) {
-        myScore.textContent=startingScore;
-        startingScore+10;
-        alert ("CORRECT!")
-    }
-    if (ans=== false) {
-        timeRemaining-10;
-        alert ("INCORRECT ANSWER")
-    }
-};
+    //     questions[0].pickOne
+    // }
+
+    //test(questions[0].pickOne, questions[0].answers)
+    next(q[2].questions[2].pickOne, questions[2].answers)
+    //create "click" function for Answer Choice//
+    clickAnswer.addEventListener("click", function () {
+        next();
+        scoreTally();
+    });
+
+    // function populateQuestion(){
+    //     questionBoxH1.textContent=questions[0].q;
+    //     choiceA.textContent=questions[0].pickOne[0].select;
+    //     choiceB.textContent=questions[0].pickOne[1].select;
+    //     choiceC.textContent=questions[0].pickOne[2].select;
+    //     choiceD.textContent=questions[0].pickOne[3].select;
+
+    // };
+
+    // function scoreTally() {
+    //     for (var i = 0; i < questions.length; i++) {
+    //     }
+    //     if (ans === true) {
+    //         myScore.textContent = startingScore;
+    //         startingScore + 10;
+    //         alert("CORRECT!")
+    //     }
+    //     if (ans === false) {
+    //         timeRemaining - 10;
+    //         alert("INCORRECT ANSWER")
+    //     }
+    // };
 
 
-register.addEventListener("click", function () {
-    highscoreBox.appendChild(init);
-   
-});
-// // {b: "Dogs Are Great", b1: "TRUE",  b2: "False"}
-// // {c: "Which of these is a dog?", c1: "Primate" c2: "Poodle" c3: "Arachnid" }
+    register.addEventListener("click", function (event) {
+        event.preventDefault();
+        highscoreBox.appendChild(init);
 
-// register.addEventListener("click", function(event){
-//    event.preventDefault();
-//    console.log(event)
-// });
+    });
+}
