61ea3660a21c4ea94593ecbfd8119797d1bc4b17
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-03 21:39:07 -0800

    create function for scoreTally (not working)- made some style changes

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 
 //#Pseudo Code//
-//1. Create a button to click -event listener- to start timer - DONE - 
-//and show question div- HELP!!!
+
 //hide start button after it is clicked once- DONE
 //have timer begin countdown- DONE
-//Create a submit button -DONE
-///with add event listner- HELP WHAT AM I DOING HERE?
-//have question div appear - HELP!!
-//create an answers array? - DONE
+///with add event listner for points and timer- HELP WHAT AM I DOING HERE?
 //2. At click, first question appears - append object???????
 //HOW DO I ORGANIZE QUESTIONS
 //HOW DO I ASSIGN SCORES?
@@ -15,10 +11,9 @@
 //3. After input, next question appears
 // 4. Wrong answers deduct 10 seconds from the clock
 // 5. Quiz ends when all questions are answered OR when time runs out.
-// 6. Quiz over - 
-//CREATE a NEW WINDOW WITH FINAL SCORE AND DIV TO input INITIALS
-//CREATE A "REGISTER YOUR SCORE" BUTTON
-//CREATE a "click"
+// 6. Quiz over when last question finished- 
+
+//CREATE A "REGISTER YOUR SCORE" BUTTON w/ move to local storage
 // High Scores into local storage
 //WINDOWS TO SHOW HIGH SCORES?
 //
@@ -29,6 +24,7 @@ var start = document.querySelector(".start")
 var register = document.querySelector("#register")
 var platter = document.querySelector(".question-platter")
 var timeRemaining = 60
+var startingScore = 0
 var answers = [];
 var questionBoxH1 = document.querySelector(".question-box")
 var choiceA = document.querySelector("#choice-a")
@@ -37,6 +33,8 @@ var choiceC = document.querySelector("#choice-c")
 var choiceD = document.querySelector("#choice-d")
 var finalScore = "";
 const timerDisplay = document.querySelector("#time-shown")
+var myScore = document.querySelector("#score")
+var clickAnswer = document.querySelector(".button-choice")
 
 
 
@@ -62,24 +60,29 @@ function countDownText() {
     }, 1000)
 }
 
-
-
 let questions = [{
     q: "What is a dog?",
     pickOne: [
-        { select: "A) a piece of wood", correct: false },
-        { select: "B) your BFF", correct: true },
-        { select: "C) a chemical", correct: false },
-        { select: "D) a vacation", correct: false }]
+        { select: "A) a piece of wood", ans: false },
+        { select: "B) your BFF", ans: true },
+        { select: "C) a chemical", ans: false },
+        { select: "D) a vacation", ans: false }]
 }, {
     q: "where is the giraffe",
     pickOne: [
-        { select: "A) a piece of wood", correct: false },
-        { select: "B) your BFF", correct: false },
-        { select: "C) a chemical", correct: false },
-        { select: "D) on vacation", correct: true}]
+        { select: "A) a piece of wood", ans: false },
+        { select: "B) your BFF", ans: false },
+        { select: "C) a chemical", ans: false },
+        { select: "D) on vacation", ans: true}]
 }, {
 }]
+clickAnswer.addEventListener("click", function () {
+    timeRemaining--;
+    countDownText();
+    // questionAppear();
+    populateQuestion();
+    scoreTally();
+});
 
 function populateQuestion(){
     questionBoxH1.textContent=questions[0].q;
@@ -87,6 +90,16 @@ function populateQuestion(){
     choiceB.textContent=questions[0].pickOne[1].select;
     choiceC.textContent=questions[0].pickOne[2].select;
     choiceD.textContent=questions[0].pickOne[3].select;
+};
+
+function scoreTally () {
+    if (ans = true) {
+        myScore.textContent=startingScore;
+        startingScore=+10;
+    }
+    if (ans = false) {
+        timeRemaining= -10;
+    }
 };
 
 
